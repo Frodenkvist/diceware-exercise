b67e673d2d288e8255cb6121ab7567bcf67357c2
Mio Engel <mioengel@gmail.com>
2020-04-29 16:52:48 +0200

    Started adding stuffs

--- src/main/kotlin/se/vbgt/diceware/Dice.kt
+++ src/main/kotlin/se/vbgt/diceware/Dice.kt
@@ -1,8 +1,11 @@
 package se.vbgt.diceware
 
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 object Dice {
     /**
      * Simulate a six sided dice (values 1 - 6)
      */
-    fun rollDice(): Int = TODO()
+    fun rollDice(): Int = Random.nextInt(1..6)
 }
--- src/main/kotlin/se/vbgt/diceware/DiceWareUtil.kt
+++ src/main/kotlin/se/vbgt/diceware/DiceWareUtil.kt
@@ -1,6 +1,35 @@
 package se.vbgt.diceware
 
+import java.nio.file.Files
+import java.nio.file.Paths
+
 object DiceWareUtil {
-    fun mapWords(vararg wordRoll: List<Int>): Map<List<Int>, String> = TODO()
-    fun randomLetter(horizontalDice: Int, verticalDice: Int): Char = TODO()
+    private val wordMap = Files.readAllLines(Paths.get(javaClass.classLoader.getResource("se/vbgt/diceware/diceware.wordlist.asc").toURI()))
+        .drop(2)
+        .take(7775)
+        .map {
+            val split = it.trim().split("\\s".toRegex())
+            split[0].map { c ->
+                Character.getNumericValue(c)
+            } to split[1]
+        }.toMap()
+
+    private val specialCharacterMatrix: Array<Array<Char>> = arrayOf(
+        arrayOf('~', '!', '#', '$', '%', '^'),
+        arrayOf('&', '*', '(', ')', '-', '='),
+        arrayOf('+', '[', ']', '\\', '{', '}'),
+        arrayOf(':', ';', '"', '\'', '<', '>'),
+        arrayOf('?', '/', '0', '1', '2', '3'),
+        arrayOf('4', '5', '6', '7', '8', '9')
+    )
+
+    fun mapWords(vararg wordRoll: List<Int>): Map<List<Int>, String> {
+        require(wordRoll.isNotEmpty())
+        require(!wordRoll.any { it.size != 5 })
+
+        return wordRoll.map {
+            it to (wordMap[it] ?: throw IllegalArgumentException())
+        }.toMap()
+    }
+    fun randomLetter(horizontalDice: Int, verticalDice: Int): Char = specialCharacterMatrix[verticalDice-1][horizontalDice-1]
 }
